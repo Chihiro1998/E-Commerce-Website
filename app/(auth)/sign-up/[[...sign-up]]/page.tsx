82136bafdf1dbedd613f1ae263ebf742d1a60bb6
Chihiro1998 <kuangyuchen0301@gmail.com>
2025-03-21 21:50:38 -0400

    updatesign-in page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,12 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div
+      className="h-screen flex justify-center items-center"
+      style={{ backgroundImage: "url('/images/auth-bg.png')" }}
+    >
+      <SignUp />
+    </div>
+  );
 }
